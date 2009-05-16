38e5b1d047ce8c5786aa5c183de083ad76499949
lucinar <lucinar@9df28ec6-a32e-4c45-89d2-a7c9c5f1e9f8>
2009-05-16 21:52:36 +0000

    Fixed some translations mistakes

--- branches/Power_Mplayer/Power_Mplayer/OptionForm.cs
+++ branches/Power_Mplayer/Power_Mplayer/OptionForm.cs
@@ -157,69 +157,103 @@ namespace Power_Mplayer
 			// 
 			// tabControl1
 			// 
+			this.tabControl1.AccessibleDescription = null;
+			this.tabControl1.AccessibleName = null;
+			resources.ApplyResources(this.tabControl1, "tabControl1");
+			this.tabControl1.BackgroundImage = null;
 			this.tabControl1.Controls.Add(this.tp_general);
 			this.tabControl1.Controls.Add(this.tp_Output);
 			this.tabControl1.Controls.Add(this.tp_Subtitle);
 			this.tabControl1.Controls.Add(this.tp_audio);
 			this.tabControl1.Controls.Add(this.tabPage1);
-			resources.ApplyResources(this.tabControl1, "tabControl1");
+			this.tabControl1.Font = null;
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
 			// 
 			// tp_general
 			// 
-			this.tp_general.Controls.Add(this.groupBox1);
+			this.tp_general.AccessibleDescription = null;
+			this.tp_general.AccessibleName = null;
 			resources.ApplyResources(this.tp_general, "tp_general");
+			this.tp_general.BackgroundImage = null;
+			this.tp_general.Controls.Add(this.groupBox1);
+			this.tp_general.Font = null;
 			this.tp_general.Name = "tp_general";
 			this.tp_general.UseVisualStyleBackColor = true;
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.AccessibleDescription = null;
+			this.groupBox1.AccessibleName = null;
+			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.BackgroundImage = null;
 			this.groupBox1.Controls.Add(this.txt_mplayer_cmd);
 			this.groupBox1.Controls.Add(this.label1);
 			this.groupBox1.Controls.Add(this.button2);
-			resources.ApplyResources(this.groupBox1, "groupBox1");
+			this.groupBox1.Font = null;
 			this.groupBox1.Name = "groupBox1";
 			this.groupBox1.TabStop = false;
 			// 
 			// txt_mplayer_cmd
 			// 
+			this.txt_mplayer_cmd.AccessibleDescription = null;
+			this.txt_mplayer_cmd.AccessibleName = null;
 			resources.ApplyResources(this.txt_mplayer_cmd, "txt_mplayer_cmd");
+			this.txt_mplayer_cmd.BackgroundImage = null;
+			this.txt_mplayer_cmd.Font = null;
 			this.txt_mplayer_cmd.Name = "txt_mplayer_cmd";
 			// 
 			// label1
 			// 
+			this.label1.AccessibleDescription = null;
+			this.label1.AccessibleName = null;
 			resources.ApplyResources(this.label1, "label1");
+			this.label1.Font = null;
 			this.label1.Name = "label1";
 			// 
 			// button2
 			// 
+			this.button2.AccessibleDescription = null;
+			this.button2.AccessibleName = null;
 			resources.ApplyResources(this.button2, "button2");
+			this.button2.BackgroundImage = null;
+			this.button2.Font = null;
 			this.button2.Name = "button2";
 			this.button2.Click += new System.EventHandler(this.btn_browse_Click);
 			// 
 			// tp_Output
 			// 
+			this.tp_Output.AccessibleDescription = null;
+			this.tp_Output.AccessibleName = null;
+			resources.ApplyResources(this.tp_Output, "tp_Output");
+			this.tp_Output.BackgroundImage = null;
 			this.tp_Output.Controls.Add(this.groupBox3);
 			this.tp_Output.Controls.Add(this.groupBox2);
-			resources.ApplyResources(this.tp_Output, "tp_Output");
+			this.tp_Output.Font = null;
 			this.tp_Output.Name = "tp_Output";
 			this.tp_Output.UseVisualStyleBackColor = true;
 			// 
 			// groupBox3
 			// 
+			this.groupBox3.AccessibleDescription = null;
+			this.groupBox3.AccessibleName = null;
+			resources.ApplyResources(this.groupBox3, "groupBox3");
+			this.groupBox3.BackgroundImage = null;
 			this.groupBox3.Controls.Add(this.Audio_Softvol_max);
 			this.groupBox3.Controls.Add(this.label4);
 			this.groupBox3.Controls.Add(this.Audio_Softvol);
 			this.groupBox3.Controls.Add(this.Audio_Output);
 			this.groupBox3.Controls.Add(this.label3);
-			resources.ApplyResources(this.groupBox3, "groupBox3");
+			this.groupBox3.Font = null;
 			this.groupBox3.Name = "groupBox3";
 			this.groupBox3.TabStop = false;
 			// 
 			// Audio_Softvol_max
 			// 
+			this.Audio_Softvol_max.AccessibleDescription = null;
+			this.Audio_Softvol_max.AccessibleName = null;
 			resources.ApplyResources(this.Audio_Softvol_max, "Audio_Softvol_max");
+			this.Audio_Softvol_max.Font = null;
 			this.Audio_Softvol_max.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -239,56 +273,89 @@ namespace Power_Mplayer
 			// 
 			// label4
 			// 
+			this.label4.AccessibleDescription = null;
+			this.label4.AccessibleName = null;
 			resources.ApplyResources(this.label4, "label4");
+			this.label4.Font = null;
 			this.label4.Name = "label4";
 			// 
 			// Audio_Softvol
 			// 
+			this.Audio_Softvol.AccessibleDescription = null;
+			this.Audio_Softvol.AccessibleName = null;
 			resources.ApplyResources(this.Audio_Softvol, "Audio_Softvol");
+			this.Audio_Softvol.BackgroundImage = null;
+			this.Audio_Softvol.Font = null;
 			this.Audio_Softvol.Name = "Audio_Softvol";
 			// 
 			// Audio_Output
 			// 
+			this.Audio_Output.AccessibleDescription = null;
+			this.Audio_Output.AccessibleName = null;
+			resources.ApplyResources(this.Audio_Output, "Audio_Output");
+			this.Audio_Output.BackgroundImage = null;
+			this.Audio_Output.Font = null;
 			this.Audio_Output.Items.AddRange(new object[] {
             resources.GetString("Audio_Output.Items"),
             resources.GetString("Audio_Output.Items1")});
-			resources.ApplyResources(this.Audio_Output, "Audio_Output");
 			this.Audio_Output.Name = "Audio_Output";
 			// 
 			// label3
 			// 
+			this.label3.AccessibleDescription = null;
+			this.label3.AccessibleName = null;
 			resources.ApplyResources(this.label3, "label3");
+			this.label3.Font = null;
 			this.label3.Name = "label3";
 			// 
 			// groupBox2
 			// 
+			this.groupBox2.AccessibleDescription = null;
+			this.groupBox2.AccessibleName = null;
+			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.BackgroundImage = null;
 			this.groupBox2.Controls.Add(this.Video_DirectRandering);
 			this.groupBox2.Controls.Add(this.Video_Output);
 			this.groupBox2.Controls.Add(this.label2);
-			resources.ApplyResources(this.groupBox2, "groupBox2");
+			this.groupBox2.Font = null;
 			this.groupBox2.Name = "groupBox2";
 			this.groupBox2.TabStop = false;
 			// 
 			// Video_DirectRandering
 			// 
+			this.Video_DirectRandering.AccessibleDescription = null;
+			this.Video_DirectRandering.AccessibleName = null;
 			resources.ApplyResources(this.Video_DirectRandering, "Video_DirectRandering");
+			this.Video_DirectRandering.BackgroundImage = null;
+			this.Video_DirectRandering.Font = null;
 			this.Video_DirectRandering.Name = "Video_DirectRandering";
 			// 
 			// Video_Output
 			// 
+			this.Video_Output.AccessibleDescription = null;
+			this.Video_Output.AccessibleName = null;
+			resources.ApplyResources(this.Video_Output, "Video_Output");
+			this.Video_Output.BackgroundImage = null;
+			this.Video_Output.Font = null;
 			this.Video_Output.Items.AddRange(new object[] {
             resources.GetString("Video_Output.Items"),
             resources.GetString("Video_Output.Items1")});
-			resources.ApplyResources(this.Video_Output, "Video_Output");
 			this.Video_Output.Name = "Video_Output";
 			// 
 			// label2
 			// 
+			this.label2.AccessibleDescription = null;
+			this.label2.AccessibleName = null;
 			resources.ApplyResources(this.label2, "label2");
+			this.label2.Font = null;
 			this.label2.Name = "label2";
 			// 
 			// tp_Subtitle
 			// 
+			this.tp_Subtitle.AccessibleDescription = null;
+			this.tp_Subtitle.AccessibleName = null;
+			resources.ApplyResources(this.tp_Subtitle, "tp_Subtitle");
+			this.tp_Subtitle.BackgroundImage = null;
 			this.tp_Subtitle.Controls.Add(this.Srt_ChineseTrans);
 			this.tp_Subtitle.Controls.Add(this.label8);
 			this.tp_Subtitle.Controls.Add(this.groupBox4);
@@ -296,39 +363,53 @@ namespace Power_Mplayer
 			this.tp_Subtitle.Controls.Add(this.label5);
 			this.tp_Subtitle.Controls.Add(this.Srt_UseASS);
 			this.tp_Subtitle.Controls.Add(this.Srt_ForceUTF8);
-			resources.ApplyResources(this.tp_Subtitle, "tp_Subtitle");
+			this.tp_Subtitle.Font = null;
 			this.tp_Subtitle.Name = "tp_Subtitle";
 			this.tp_Subtitle.UseVisualStyleBackColor = true;
 			// 
 			// Srt_ChineseTrans
 			// 
+			this.Srt_ChineseTrans.AccessibleDescription = null;
+			this.Srt_ChineseTrans.AccessibleName = null;
+			resources.ApplyResources(this.Srt_ChineseTrans, "Srt_ChineseTrans");
+			this.Srt_ChineseTrans.BackgroundImage = null;
+			this.Srt_ChineseTrans.Font = null;
 			this.Srt_ChineseTrans.FormattingEnabled = true;
 			this.Srt_ChineseTrans.Items.AddRange(new object[] {
             resources.GetString("Srt_ChineseTrans.Items"),
             resources.GetString("Srt_ChineseTrans.Items1"),
             resources.GetString("Srt_ChineseTrans.Items2")});
-			resources.ApplyResources(this.Srt_ChineseTrans, "Srt_ChineseTrans");
 			this.Srt_ChineseTrans.Name = "Srt_ChineseTrans";
 			// 
 			// label8
 			// 
+			this.label8.AccessibleDescription = null;
+			this.label8.AccessibleName = null;
 			resources.ApplyResources(this.label8, "label8");
+			this.label8.Font = null;
 			this.label8.Name = "label8";
 			// 
 			// groupBox4
 			// 
+			this.groupBox4.AccessibleDescription = null;
+			this.groupBox4.AccessibleName = null;
+			resources.ApplyResources(this.groupBox4, "groupBox4");
+			this.groupBox4.BackgroundImage = null;
 			this.groupBox4.Controls.Add(this.Srt_SubFontTextScale);
 			this.groupBox4.Controls.Add(this.label7);
 			this.groupBox4.Controls.Add(this.btn_BroseFont);
 			this.groupBox4.Controls.Add(this.label6);
 			this.groupBox4.Controls.Add(this.Srt_Font);
-			resources.ApplyResources(this.groupBox4, "groupBox4");
+			this.groupBox4.Font = null;
 			this.groupBox4.Name = "groupBox4";
 			this.groupBox4.TabStop = false;
 			// 
 			// Srt_SubFontTextScale
 			// 
+			this.Srt_SubFontTextScale.AccessibleDescription = null;
+			this.Srt_SubFontTextScale.AccessibleName = null;
 			resources.ApplyResources(this.Srt_SubFontTextScale, "Srt_SubFontTextScale");
+			this.Srt_SubFontTextScale.Font = null;
 			this.Srt_SubFontTextScale.Name = "Srt_SubFontTextScale";
 			this.Srt_SubFontTextScale.Value = new decimal(new int[] {
             4,
@@ -338,73 +419,115 @@ namespace Power_Mplayer
 			// 
 			// label7
 			// 
+			this.label7.AccessibleDescription = null;
+			this.label7.AccessibleName = null;
 			resources.ApplyResources(this.label7, "label7");
+			this.label7.Font = null;
 			this.label7.Name = "label7";
 			// 
 			// btn_BroseFont
 			// 
+			this.btn_BroseFont.AccessibleDescription = null;
+			this.btn_BroseFont.AccessibleName = null;
 			resources.ApplyResources(this.btn_BroseFont, "btn_BroseFont");
+			this.btn_BroseFont.BackgroundImage = null;
+			this.btn_BroseFont.Font = null;
 			this.btn_BroseFont.Name = "btn_BroseFont";
 			this.btn_BroseFont.UseVisualStyleBackColor = true;
 			this.btn_BroseFont.Click += new System.EventHandler(this.btn_BroseFont_Click);
 			// 
 			// label6
 			// 
+			this.label6.AccessibleDescription = null;
+			this.label6.AccessibleName = null;
 			resources.ApplyResources(this.label6, "label6");
+			this.label6.Font = null;
 			this.label6.Name = "label6";
 			// 
 			// Srt_Font
 			// 
+			this.Srt_Font.AccessibleDescription = null;
+			this.Srt_Font.AccessibleName = null;
 			resources.ApplyResources(this.Srt_Font, "Srt_Font");
+			this.Srt_Font.BackgroundImage = null;
+			this.Srt_Font.Font = null;
 			this.Srt_Font.Name = "Srt_Font";
 			// 
 			// Srt_AutoScale
 			// 
+			this.Srt_AutoScale.AccessibleDescription = null;
+			this.Srt_AutoScale.AccessibleName = null;
+			resources.ApplyResources(this.Srt_AutoScale, "Srt_AutoScale");
+			this.Srt_AutoScale.BackgroundImage = null;
+			this.Srt_AutoScale.Font = null;
 			this.Srt_AutoScale.FormattingEnabled = true;
 			this.Srt_AutoScale.Items.AddRange(new object[] {
             resources.GetString("Srt_AutoScale.Items"),
             resources.GetString("Srt_AutoScale.Items1"),
             resources.GetString("Srt_AutoScale.Items2"),
             resources.GetString("Srt_AutoScale.Items3")});
-			resources.ApplyResources(this.Srt_AutoScale, "Srt_AutoScale");
 			this.Srt_AutoScale.Name = "Srt_AutoScale";
 			// 
 			// label5
 			// 
+			this.label5.AccessibleDescription = null;
+			this.label5.AccessibleName = null;
 			resources.ApplyResources(this.label5, "label5");
+			this.label5.Font = null;
 			this.label5.Name = "label5";
 			// 
 			// Srt_UseASS
 			// 
+			this.Srt_UseASS.AccessibleDescription = null;
+			this.Srt_UseASS.AccessibleName = null;
 			resources.ApplyResources(this.Srt_UseASS, "Srt_UseASS");
+			this.Srt_UseASS.BackgroundImage = null;
+			this.Srt_UseASS.Font = null;
 			this.Srt_UseASS.Name = "Srt_UseASS";
 			this.Srt_UseASS.UseVisualStyleBackColor = true;
 			// 
 			// Srt_ForceUTF8
 			// 
+			this.Srt_ForceUTF8.AccessibleDescription = null;
+			this.Srt_ForceUTF8.AccessibleName = null;
+			resources.ApplyResources(this.Srt_ForceUTF8, "Srt_ForceUTF8");
+			this.Srt_ForceUTF8.BackgroundImage = null;
 			this.Srt_ForceUTF8.Checked = true;
 			this.Srt_ForceUTF8.CheckState = System.Windows.Forms.CheckState.Checked;
-			resources.ApplyResources(this.Srt_ForceUTF8, "Srt_ForceUTF8");
+			this.Srt_ForceUTF8.Font = null;
 			this.Srt_ForceUTF8.Name = "Srt_ForceUTF8";
 			// 
 			// tp_audio
 			// 
+			this.tp_audio.AccessibleDescription = null;
+			this.tp_audio.AccessibleName = null;
+			resources.ApplyResources(this.tp_audio, "tp_audio");
+			this.tp_audio.BackgroundImage = null;
 			this.tp_audio.Controls.Add(this.btn_SelectAllExt);
 			this.tp_audio.Controls.Add(this.clb_FileAssociate);
-			resources.ApplyResources(this.tp_audio, "tp_audio");
+			this.tp_audio.Font = null;
 			this.tp_audio.Name = "tp_audio";
 			this.tp_audio.UseVisualStyleBackColor = true;
 			// 
 			// btn_SelectAllExt
 			// 
+			this.btn_SelectAllExt.AccessibleDescription = null;
+			this.btn_SelectAllExt.AccessibleName = null;
 			resources.ApplyResources(this.btn_SelectAllExt, "btn_SelectAllExt");
+			this.btn_SelectAllExt.BackgroundImage = null;
+			this.btn_SelectAllExt.Font = null;
 			this.btn_SelectAllExt.Name = "btn_SelectAllExt";
 			this.btn_SelectAllExt.UseVisualStyleBackColor = true;
 			this.btn_SelectAllExt.Click += new System.EventHandler(this.btn_SelectAllExt_Click);
 			// 
 			// clb_FileAssociate
 			// 
+			this.clb_FileAssociate.AccessibleDescription = null;
+			this.clb_FileAssociate.AccessibleName = null;
+			resources.ApplyResources(this.clb_FileAssociate, "clb_FileAssociate");
+			this.clb_FileAssociate.BackgroundImage = null;
 			this.clb_FileAssociate.CheckOnClick = true;
+			this.clb_FileAssociate.Font = null;
 			this.clb_FileAssociate.FormattingEnabled = true;
 			this.clb_FileAssociate.Items.AddRange(new object[] {
             resources.GetString("clb_FileAssociate.Items"),
@@ -477,53 +600,80 @@ namespace Power_Mplayer
             resources.GetString("clb_FileAssociate.Items67"),
             resources.GetString("clb_FileAssociate.Items68"),
             resources.GetString("clb_FileAssociate.Items69")});
-			resources.ApplyResources(this.clb_FileAssociate, "clb_FileAssociate");
 			this.clb_FileAssociate.Name = "clb_FileAssociate";
 			// 
 			// tabPage1
 			// 
+			this.tabPage1.AccessibleDescription = null;
+			this.tabPage1.AccessibleName = null;
+			resources.ApplyResources(this.tabPage1, "tabPage1");
+			this.tabPage1.BackgroundImage = null;
 			this.tabPage1.Controls.Add(this.chkLanguage);
 			this.tabPage1.Controls.Add(this.label9);
-			resources.ApplyResources(this.tabPage1, "tabPage1");
+			this.tabPage1.Font = null;
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.UseVisualStyleBackColor = true;
 			// 
 			// chkLanguage
 			// 
+			this.chkLanguage.AccessibleDescription = null;
+			this.chkLanguage.AccessibleName = null;
+			resources.ApplyResources(this.chkLanguage, "chkLanguage");
+			this.chkLanguage.BackgroundImage = null;
+			this.chkLanguage.Font = null;
 			this.chkLanguage.FormattingEnabled = true;
 			this.chkLanguage.Items.AddRange(new object[] {
             resources.GetString("chkLanguage.Items"),
             resources.GetString("chkLanguage.Items1"),
             resources.GetString("chkLanguage.Items2"),
             resources.GetString("chkLanguage.Items3")});
-			resources.ApplyResources(this.chkLanguage, "chkLanguage");
 			this.chkLanguage.Name = "chkLanguage";
 			// 
 			// label9
 			// 
+			this.label9.AccessibleDescription = null;
+			this.label9.AccessibleName = null;
 			resources.ApplyResources(this.label9, "label9");
+			this.label9.Font = null;
 			this.label9.Name = "label9";
 			// 
 			// btn_close
 			// 
-			this.btn_close.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btn_close.AccessibleDescription = null;
+			this.btn_close.AccessibleName = null;
 			resources.ApplyResources(this.btn_close, "btn_close");
+			this.btn_close.BackgroundImage = null;
+			this.btn_close.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.btn_close.Font = null;
 			this.btn_close.Name = "btn_close";
 			this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
 			// 
+			// openFileDialog1
+			// 
+			resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+			// 
 			// btn_cancel
 			// 
-			this.btn_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btn_cancel.AccessibleDescription = null;
+			this.btn_cancel.AccessibleName = null;
 			resources.ApplyResources(this.btn_cancel, "btn_cancel");
+			this.btn_cancel.BackgroundImage = null;
+			this.btn_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btn_cancel.Font = null;
 			this.btn_cancel.Name = "btn_cancel";
 			// 
 			// OptionForm
 			// 
+			this.AccessibleDescription = null;
+			this.AccessibleName = null;
 			resources.ApplyResources(this, "$this");
+			this.BackgroundImage = null;
 			this.Controls.Add(this.btn_cancel);
 			this.Controls.Add(this.btn_close);
 			this.Controls.Add(this.tabControl1);
+			this.Font = null;
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Icon = null;
 			this.Name = "OptionForm";
 			this.Load += new System.EventHandler(this.OptionForm_Load);
 			this.tabControl1.ResumeLayout(false);
@@ -729,6 +879,9 @@ namespace Power_Mplayer
 					break;
 				case "zh-CHT":
 					chkLanguage.SelectedIndex = 2;
+					break;
+				case "pt":
+					chkLanguage.SelectedIndex = 3;
 					break;
 			}
 		}
